4eaa696f9e85ef14d93d9cfc71b8cc4a6aa162ef
laierr <kenderberator@gmail.com>
2019-01-23 17:24:54 +0200

    clean up

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
